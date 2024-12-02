99537e9a64ed75441a4515a543064cda31e101a4
saniya <saniyasingh@gmail.com>
2024-12-02 00:10:28 -0500

    update route for trip setup

--- src/app/api/trips/[id]/route.js
+++ src/app/api/trips/[id]/route.js
@@ -1,15 +1,40 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase-config";
-import { doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 
+export async function PUT(req, context) {
+    const params = await context.params;
+    const { id } = params;
+    const updateFields = await req.json();
+    // console.log('ID:', id);
+    // console.log(updateFields);
 
-export async function PUT(request){
+    try{
 
-    const { id } = request.query;
+        if(!id){
+            return NextResponse.json({success: false, message: "id not defined"}, {status: 400});
+        }
 
+        if(Object.keys(updateFields).length === 0){
+            return NextResponse.json({success: false, message: "update fields not defined"}, {status: 400});
+        }
 
+        const tripRef = doc(db, 'trips', id);
+        await updateDoc(tripRef, updateFields);
+
+        return NextResponse.json({success: true}, {status: 200});
+
+    } catch(error){
+        console.error(error);
+        return NextResponse.json({success: false, message: error.message}, {status: 500});
+    }
+    
 }
 
-export async function DELETE(request){
+export async function DELETE(req, context){
+    const params = await context.params;
+    const { id } = params;
+    console.log('ID:', id);
 
+    return NextResponse.json({success: true}, {status: 200});
 }
