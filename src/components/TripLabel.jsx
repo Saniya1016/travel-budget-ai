80385c1908a60455e1aceecd01d784cf4f874019
saniya <saniyasingh@gmail.com>
2024-12-15 18:17:36 -0500

    UI changes

--- src/components/TripLabel.jsx
+++ src/components/TripLabel.jsx
@@ -1,9 +1,15 @@
-
+"use client";
 
 export default function TripLabel({trip}) {
+
+    const handleViewTrip = async() => {
+      console.log("view trip");
+    }
+
   return (
     <div
-        className="bg-gray-800 shadow-md rounded-lg p-6 border border-gray-700 hover:shadow-lg hover:scale-105 transition transform duration-300"
+      className="bg-gray-800 shadow-md rounded-lg p-6 border border-gray-700 cursor-pointer hover:shadow-lg hover:scale-105 transition transform duration-300"
+      
     >
         <h3 className="text-xl font-semibold text-white mb-2">
         {trip.destination}
@@ -15,6 +21,12 @@ export default function TripLabel({trip}) {
         <span className="font-medium">Dates:</span> {trip.FromDate} -{" "}
         {trip.ToDate}
         </p>
+        <div className="flex items-center gap-2 mt-4">
+          <span onClick={handleViewTrip} className="px-3 py-1 bg-green-500 text-white text-xs rounded-lg">
+            View Details
+          </span>
+        </div>
+
     </div>
   )
 }
