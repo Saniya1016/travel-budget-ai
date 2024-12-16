473d2f57826e064942391be24ceec2c3452c2d0f
saniya <saniyasingh@gmail.com>
2024-12-16 01:32:59 -0500

    form to add trip details-in progress

--- src/app/dashboard/tripForm/page.jsx
+++ src/app/dashboard/tripForm/page.jsx
@@ -1,17 +1,48 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import DatePicker from "react-datepicker";
+
 
 export default function page() {
 
-    const startDate = useRef(null);
-    const endDate = useRef(null);
-    const budget = useRef(null);
-    const destination = useRef(null);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
+
+    const budgetRef = useRef(null);
+    const destinationRef = useRef(null);
 
   return (
     <div>
-      
+      <h1>Enter Your Trip Details</h1>
+      <form>
+        <div>
+            <label>Destination</label>
+            <input
+                type="text"
+                id="destination"
+                ref={destinationRef}
+                placeholder="Enter destination"
+                required
+            />
+        </div>
+
+        <div>
+            <label>Budget($)</label>
+            <input
+                type="number"
+                id="budget"
+                ref={budgetRef}
+                placeholder="Enter budget"
+                required
+            />
+        </div>
+
+        <button type="submit">
+            Create Trip
+        </button>
+
+      </form>
     </div>
   )
 }
