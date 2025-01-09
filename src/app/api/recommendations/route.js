beb46a9eb2f60cc38bcdbb29011db0ca18bc3b96
saniya <saniyasingh@gmail.com>
2025-01-09 17:22:01 -0500

    started planning recommendations feature

---
+++ src/app/api/recommendations/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+//first get input body - start, end dates, budget, destination, preferences : {for now food - 50% activities-50%}
+//send details to Google api
+//get recommendations
+
+//send the recommendations to openai with prompt
+//fetch grouped results by day
